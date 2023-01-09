e5f4469d97686459025804c499d7cf32882eb5d4
arati <arati@Arati>
2023-01-09 14:19:37 +0530

    [arati]:project created

---
+++ src/Githubtool/sample1.java
@@ -0,0 +1,10 @@
+package Githubtool;
+
+public class sample1 
+{
+public static void main(String[] args) 
+{System.out.println("hi ");
+System.out.println("hello");
+	
+}
+}
---
+++ src/Githubtool/sample2.java
@@ -0,0 +1,10 @@
+package Githubtool;
+
+public class sample2 
+{public static void main(String[] args)
+{
+	System.out.println("arati nilesh panchariya");
+	System.out.println("pune");
+}
+
+}
